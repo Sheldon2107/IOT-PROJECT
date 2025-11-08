a1a979cc8daaaffb45d5edcc114a3085c8b53732
Sheldon2107 <122601242+Sheldon2107@users.noreply.github.com>
2025-11-08 16:33:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
-from db import init_database, get_last_3days
+from db import init_database, get_last_n_days
 
-app = Flask(__name__, static_folder="public")
+app = Flask(__name__, template_folder='.')
 CORS(app)
 
-# Initialize DB
+# Initialize database on startup
 init_database()
 
 @app.route('/')
 def index():
-    return send_from_directory(app.static_folder, 'index.html')
+    return render_template('index.html')
 
 @app.route('/api/last3days')
-def last_3days():
-    data = get_last_3days()
+def last_3_days():
+    data = get_last_n_days(3)
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
