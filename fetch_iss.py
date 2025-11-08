016a61c129f85d5cd2a8f7324dfb32971c9c33a3
Sheldon2107 <122601242+Sheldon2107@users.noreply.github.com>
2025-11-08 17:37:21 +0800

    Update fetch_iss.py

--- fetch_iss.py
+++ fetch_iss.py
@@ -1,6 +1,8 @@
 import requests
 import time
-from db import insert_position
+from db import insert_position, init_db
+
+init_db()
 
 API_URL = "https://api.wheretheiss.at/v1/satellites/25544"
 
@@ -15,12 +17,12 @@ def fetch_and_store():
                 alt = data["altitude"]
                 ts_utc = data["timestamp"]
                 insert_position(lat, lon, alt, ts_utc)
-                print(f"Saved: Lat {lat}, Lon {lon}, Alt {alt}")
+                print(f"Saved: Lat {lat:.4f}, Lon {lon:.4f}, Alt {alt:.2f} km")
             else:
                 print(f"Error fetching ISS data: {res.status_code}")
         except Exception as e:
             print("Error:", e)
-        time.sleep(60)  # Fetch every 1 minute
+        time.sleep(60)  # fetch every 1 minute
 
 if __name__ == "__main__":
     fetch_and_store()
